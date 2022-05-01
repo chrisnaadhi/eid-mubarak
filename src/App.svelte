17e371cae935ee1de037abfefa3e98d69124b70e
chrisnaadhi <chrisnaadhip@gmail.com>
2022-05-01 23:40:28 +0700

    fix path

--- src/App.svelte
+++ src/App.svelte
@@ -70,7 +70,7 @@
 
   main {
     height: 100%;
-    background-image: url("/assets/bg.jpg");
+    background-image: url("bg.jpg");
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
